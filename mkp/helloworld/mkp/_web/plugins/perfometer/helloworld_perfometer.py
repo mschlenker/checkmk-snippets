24c2d7ddc87f0350d7a5c1dffb5364e545386752
Mattias Schlenker <ms@mattiasschlenker.de>
2021-10-07 09:06:10 +0200

    add hello world template

---
+++ mkp/helloworld/mkp/_web/plugins/perfometer/helloworld_perfometer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Mattias Schlenker <ms@mattiasschlenker.de> for tribe29 GmbH
+# License: GNU General Public License v2
+#
+# Reference for details:
+# https://docs.checkmk.com/latest/en/devel_check_plugins.html
+#
+# Configuration for a simple perf-o-meter that displays percentage
+
+from cmk.gui.plugins.metrics import perfometer_info
+
+
+# Just create the most simple perf-o-meter displaying only one linear value.
+# We use the variable "hellolevel" as reference. Since output ranges from 0
+# to 100 we just use the full range.
+
+perfometer_info.append({
+    "type": "linear",
+    "segments": ["hellolevel"],
+    "total": 100.0,
+})
+
