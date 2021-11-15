f5926e46d75be21f09a00ecde7b338bc624972aa
Mattias Schlenker <ms@mattiasschlenker.de>
2021-11-15 16:58:17 +0100

    add script to create really large screenshots

---
+++ scripts/megax.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 
+# Start Firefox in a large dummy display to be able to get high resolution
+# screenshots.
+
+# Other suggestions: xterm, xfce4-terminal
+TERMINAL=urxvt
+# Alternative suggestions: openbox, i3, fluxbox, twm, icewm
+WINMANAGER=xfwm4
+WIDTH=3840
+HEIGHT=3840
+DPI=96
+DPNUM=":7"
+FFPROFILE=CMK
+
+Xvfb ${DPNUM} -retro -nolisten tcp -dpi ${DPI} -screen ${DPNUM} ${WIDTH}x${HEIGHT}x24 &
+sleep 3
+
+DISPLAY=${DPNUM} ${TERMINAL} &
+DISPLAY=${DPNUM} firefox -P ${FFPROFILE} -no-remote &
+# Might be redundant, I don't care...
+DISPLAY=${DPNUM} x11vnc -loop -display ${DPNUM} &
+DISPLAY=${DPNUM} ${WINMANAGER} &
+
+# Start xfsettingsd here if needed. You can configure the appearance (icons,
+# theme, colors, fonts) with xfce4-settings-manager 
+
+# Now use any usable VNC viewer to access the desktop. I suggest Remmina,
+# since this allows scaling. 
+#
+# To take screenshots you can run in any terminal:
+#
+# DISPLAY=:7 scrot /tmp/mycmkscreenshot.png 
+#
+# Run in a terminal of its own. Or to kill: Close firefox, then first kill
+# x11vnc, then kill Xvfb.
+#
+# Have fun! Mattias 
