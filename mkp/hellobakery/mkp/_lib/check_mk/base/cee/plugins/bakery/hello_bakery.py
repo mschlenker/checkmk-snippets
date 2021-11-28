58e43d7b5b0a4414ca46307919501d3b2fcbd09b
Mattias Schlenker <ms@mattiasschlenker.de>
2021-11-28 15:58:43 +0100

    update comments

--- mkp/hellobakery/mkp/_lib/check_mk/base/cee/plugins/bakery/hello_bakery.py
+++ mkp/hellobakery/mkp/_lib/check_mk/base/cee/plugins/bakery/hello_bakery.py
@@ -11,7 +11,7 @@
 
 import json
 from pathlib import Path
-from typing import Iterable, TypedDict, List
+from typing import TypedDict, List
 
 # Import a whole lot of the bakery API. This might be too much, but probably will
 # help you when extending our example.
@@ -23,7 +23,7 @@ from .bakery_api.v1 import (
     SolStep,
     Plugin,
     PluginConfig,
-    SystemBinary,
+    # SystemBinary, # add when using the commented code parts
     Scriptlet,
     WindowsConfigEntry,
     register,
@@ -33,84 +33,88 @@ from .bakery_api.v1 import (
     quote_shell_string,
 )
 
+# Create a class that holds our config. This corresponds to the parameters set
+# in the setup GUI and defines in web/plugins/hellobakery_bakery.py.
+
 class HelloBakeryConfig(TypedDict, total=False):
-   interval: int
-   user: str
-   content: str
+    interval: int
+    user: str
+    content: str
 
 def get_hello_bakery_plugin_files(conf: HelloBakeryConfig) -> FileGenerator:
-   # In some cases you may want to override user input here to ensure a minimal
-   # interval!
-   interval = conf.get('interval')
-
-   # Source file with this name is taken from ~/local/share/check_mk/agents/plugins/
-   # It will be installed under this name in /usr/lib/check_mk_agent/plugins/<interval>/
-   # or in /usr/lib/check_mk_agent/plugins/ (if synchronous call is requested)
-   # on the target system.
-   yield Plugin(
-      base_os=OS.LINUX,
-      source=Path('hello_bakery'),
-      target=Path('hello_bakery'),
-      interval=interval,
-   )
-   
-   # This example skips an agent plugin for Solaris systems. If unsure whether
-   # Python is present, you might want to add a Korn shell script instead:
-   #
-   #yield Plugin(
-   #   base_os=OS.SOLARIS,
-   #   source=Path('hello_bakery.solaris.ksh'),
-   #   target=Path('hello_bakery'),
-   #   interval=interval,
-   #)
-   
-   # Install a CMD file for Windows (BAT/PS/VMS are recommended defaults):
-   yield Plugin(
-      base_os=OS.WINDOWS,
-      source=Path('hello_bakery.cmd'),
-      target=Path('hello_bakery.bat'),
-      interval=interval,
-   )
-   
-   # Put a configuration file to the list for Linux systems:
-   # Switch of the banner, since it uses hash as comment.
-   yield PluginConfig(base_os=OS.LINUX,
-                     lines=_get_linux_cfg_lines(conf['user'], conf['content']),
-                     target=Path('hello_bakery.json'),
-                     include_header=False)
-   
-   # Put a configuration file to the list for Solaris systems:
-   # If we build a configuration file that can be sourced as shell snippet, we can
-   # keep the banner:
-   #
-   #yield PluginConfig(base_os=OS.SOLARIS,
-   #                  lines=_get_solaris_cfg_lines(conf['user'], conf['content']),
-   #                  target=Path('hello_bakery.cfg'),
-   #                  include_header=True)
-
-   # In some cases the agent needs to be accompanied by a binary. This dumps
-   # the binary mentioned to the default binary directionary (typically /usr/bin)
-   # and registers the file with the package manager.
-   #
-   #for base_os in [OS.LINUX]:
-   #   yield SystemBinary(
-   #      base_os=base_os,
-   #      source=Path('some_binary'),
-   #   )
-
+    # In some cases you may want to override user input here to ensure a minimal
+    # interval!
+    interval = conf.get('interval')
+    
+    # The source file, specified with "source" argument, is taken from
+    # ~/local/share/check_mk/agents/plugins/. It will be installed under the target name,
+    # specified with "target" argument, in /usr/lib/check_mk_agent/plugins/<interval>/
+    # or in /usr/lib/check_mk_agent/plugins/ (if synchronous call is requested)
+    # on the target system. If the "target" argument is omitted, the "source" argument
+    # will be reused as target name
+    yield Plugin(
+        base_os=OS.LINUX,
+        source=Path('hello_bakery'),
+        interval=interval,
+    )
+    
+    # This example skips an agent plugin for Solaris systems. If unsure whether
+    # Python is present, you might want to add a Korn shell script instead:
+    #
+    #yield Plugin(
+    #   base_os=OS.SOLARIS,
+    #   source=Path('hello_bakery.solaris.ksh'),
+    #   target=Path('hello_bakery'),
+    #   interval=interval,
+    #)
+    
+    # Install a CMD file for Windows (BAT/PS/VMS are recommended defaults):
+    yield Plugin(
+        base_os=OS.WINDOWS,
+        source=Path('hello_bakery.cmd'),
+        target=Path('hello_bakery.bat'),
+        interval=interval,
+    )
+    
+    # Put a configuration file to the list for Linux systems: Here we assume that our plugin
+    # uses a JSON file for configuration. Switch off the banner, since the banner would use
+    # shell style comments (#), but JSON requires JavaScript like comments (/* ... */).
+    yield PluginConfig(base_os=OS.LINUX,
+                      lines=_get_linux_cfg_lines(conf['user'], conf['content']),
+                      target=Path('hello_bakery.json'),
+                      include_header=False)
+    
+    # Put a configuration file to the list for Solaris systems:
+    # If we build a configuration file that can be sourced as shell snippet, we can
+    # keep the banner:
+    #
+    #yield PluginConfig(base_os=OS.SOLARIS,
+    #                  lines=_get_solaris_cfg_lines(conf['user'], conf['content']),
+    #                  target=Path('hello_bakery.cfg'),
+    #                  include_header=True)
+    
+    # In some cases the agent needs to be accompanied by a binary. This dumps
+    # the binary mentioned to the default binary directionary (typically /usr/bin)
+    # and registers the file with the package manager.
+    #
+    #for base_os in [OS.LINUX]:
+    #   yield SystemBinary(
+    #      base_os=base_os,
+    #      source=Path('some_binary'),
+    #   )
 
 def _get_linux_cfg_lines(user: str, content: str) -> List[str]:
-   # Let's assume that our Linux example plugin uses JSON as configuration file format
-   config = json.dumps({'user': user, 'content': content})
-   return config.split('\n')
+    # Let's assume that our Linux example plugin uses JSON as configuration file format
+    config = json.dumps({'user': user, 'content': content})
+    return config.split('\n')
 
 def _get_solaris_cfg_lines(user: str, content: str) -> List[str]:
-   # To be loaded with 'source' in Solaris shell script
-   return [
-      f'USER={quote_shell_string(user)}',
-      f'CONTENT={quote_shell_string(user)}',
-   ]
-
+    # To be loaded with 'source' in Solaris shell script
+    return [
+        f'USER={quote_shell_string(user)}',
+        f'CONTENT={quote_shell_string(user)}',
+    ]
+    
 # Depending on your preference you might wanna use pickle to dump the config
 # or write plain CSV... It's all up to you. Just make sure that configuration files
 # are always treated as an array of lines.
@@ -127,28 +131,27 @@ def _get_solaris_cfg_lines(user: str, content: str) -> List[str]:
 # directory, save the current directory as olddir=`pwd` and change back as last line.
 
 def get_hello_bakery_scriptlets(conf: HelloBakeryConfig) -> ScriptletGenerator:
-   installed_lines = ['logger -p Checkmk_Agent "Installed hello_bakery"']
-   uninstalled_lines = ['logger -p Checkmk_Agent "Uninstalled hello_bakery"']
-
-   yield Scriptlet(step=DebStep.POSTINST, lines=installed_lines)
-   yield Scriptlet(step=DebStep.POSTRM, lines=uninstalled_lines)
-   yield Scriptlet(step=RpmStep.POST, lines=installed_lines)
-   yield Scriptlet(step=RpmStep.POSTUN, lines=uninstalled_lines)
-   yield Scriptlet(step=SolStep.POSTINSTALL, lines=installed_lines)
-   yield Scriptlet(step=SolStep.POSTREMOVE, lines=uninstalled_lines)
+    installed_lines = ['logger -p Checkmk_Agent "Installed hello_bakery"']
+    uninstalled_lines = ['logger -p Checkmk_Agent "Uninstalled hello_bakery"']
+    
+    yield Scriptlet(step=DebStep.POSTINST, lines=installed_lines)
+    yield Scriptlet(step=DebStep.POSTRM, lines=uninstalled_lines)
+    yield Scriptlet(step=RpmStep.POST, lines=installed_lines)
+    yield Scriptlet(step=RpmStep.POSTUN, lines=uninstalled_lines)
+    yield Scriptlet(step=SolStep.POSTINSTALL, lines=installed_lines)
+    yield Scriptlet(step=SolStep.POSTREMOVE, lines=uninstalled_lines)
 
 # Just because we can we will also write a Windows configuration. In contrast to
 # Unices, Windows configuration is kept in a centralized file, not in
 # individual files for each plugin.
 
 def get_hello_bakery_windows_config(conf: HelloBakeryConfig) -> WindowsConfigGenerator:
-   yield WindowsConfigEntry(path=["hello_bakery", "user"], content=conf["user"])
-   yield WindowsConfigEntry(path=["hello_bakery", "content"], content=conf["content"])
+    yield WindowsConfigEntry(path=["hello_bakery", "user"], content=conf["user"])
+    yield WindowsConfigEntry(path=["hello_bakery", "content"], content=conf["content"])
 
 register.bakery_plugin(
-   name="hello_bakery",
-   files_function=get_hello_bakery_plugin_files,
-   scriptlets_function=get_hello_bakery_scriptlets,
-   windows_config_function=get_hello_bakery_windows_config,
+    name="hello_bakery",
+    files_function=get_hello_bakery_plugin_files,
+    scriptlets_function=get_hello_bakery_scriptlets,
+    windows_config_function=get_hello_bakery_windows_config,
 )
-
