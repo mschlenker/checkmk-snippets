4e11d5db736bdb185f234b60322842297e32ddbb
Mattias Schlenker <ms@mattiasschlenker.de>
2022-06-16 16:50:56 +0200

    massive cleanup

--- docserve/docserve.rb
+++ docserve/docserve.rb
@@ -14,9 +14,10 @@ require 'nokogiri'
 $basepath = nil # Path to the checkmk-docs directory
 $templates = nil # Path to the checkmkdocs-styling directory
 $cachedir = nil # Path to the cache directory, needed for the menu 
+$port = 8088 # Port to use
 $cachedfiles = Hash.new
 
-# FIXME: Currently we are limited to one branch
+# FIXME later: Currently we are limited to one branch
 $branches = "localdev"
 $latest = "localdev"
 $onthispage = {
@@ -31,8 +32,68 @@ $menufrags = {
 	"de" => nil,
 	"en" => nil
 }
+$mimetypes = {
+	"html" => "text/html",
+	"css" => "text/css",
+	"js" => "text/javascript",
+	"woff" => "font/woff",
+	"woff2" => "font/woff2",
+	"eot" => "application/vnd.ms-fontobject",
+	"png" => "image/png",
+	"jpg" => "image/jpeg",
+	"jpeg" => "image/jpeg",
+	"svg" => "image/svg+xml"
+}
 
-# $css = File.read("default.css")
+$allowed = [] # Store a complete list of all request paths
+$html = [] # Store a list of all HTML files
+
+# Create a list of all allowed files:
+def create_filelist
+	# Allow all asciidoc files except includes and menus
+	$onthispage.each { |lang, s| 
+		Dir.entries($basepath + "/" + lang).each { |f|
+			if f =~ /\.asciidoc/ 
+				fname = "/latest/" + lang + "/" + f.sub(/\.asciidoc$/, ".html")
+				unless f =~ /^(include|menu)/
+					$allowed.push fname
+					$html.push fname
+				end
+			end
+		}
+	}
+	# Allow all images, but change their paths to include the language
+	Dir.entries($basepath + "/images").each { |f|
+		if f =~ /\.(png|jpeg|jpg|svg)$/
+			$allowed.push "/latest/images/" + f
+		end
+	}
+	# Allow all icons
+	Dir.entries($basepath + "/images/icons").each { |f|
+		if f =~ /\.(png|jpeg|jpg|svg)$/
+			$allowed.push "/latest/images/icons/" + f
+		end
+	}
+	# Allow all files in any subdirectory in assets
+	Dir.entries($templates + "/assets").each { |d|
+		if File.directory?($templates + "/assets/" + d)
+			unless d =~ /^\./
+				Dir.entries($templates + "/assets/" + d).each { |f|
+					$allowed.push "/assets/" + d + "/" + f if File.file?($templates + "/assets/" + d + "/" + f)
+				}
+			end
+		end
+	}
+	# Allow the lunr index
+	$onthispage.each { |lang, s| 
+		$allowed.push "/latest/lunr.index.#{lang}.js"
+	}
+	$allowed.push "/latest/index.html"
+	$allowed.push "/latest/"
+	$allowed.push "/latest"
+	$allowed.each { |f| $stderr.puts f }
+end
+
 
 def prepare_cache
 	[ "de", "en" ].each { |l|
@@ -40,6 +101,15 @@ def prepare_cache
 	}
 	[ "js", "css" ].each { |l|
 		FileUtils.mkdir_p($cachedir + "/assets/" + l )
+	}
+end
+
+# Do initial caching of the menu:
+def prepare_menu
+	[ "de", "en" ].each { |lang|
+		path = "/#{lang}/menu.asciidoc"
+		s = SingleDocFile.new path
+		$cachedfiles[path] = s
 	}
 end
 
@@ -65,6 +135,8 @@ class SingleDocFile
 	@html = nil
 	@mtime = nil
 	@filename = nil
+	@lang = "en"
+	@blocked = false # Make sure no concurrent asciidoctor processes are running
 	
 	# Initialize, first read
 	def initialize(filename)
@@ -72,78 +144,64 @@ class SingleDocFile
 		reread
 	end
 	
-	def rebuild_menu
-	# Build the menu if necessary
-		[ "de", "en" ].each { |l|
-			rebuild = true
-			unless $menuage[l].nil?
-				rebuild = false if File.mtime("#{$basepath}/#{l}/menu.asciidoc") <= $menuage[l]
-			end
-			if rebuild
-				system("asciidoctor -T \"#{$templates}/templates/index\" -E slim \"#{$basepath}/#{l}/menu.asciidoc\" -D \"#{$cachedir}/#{$latest}/#{l}\"")
-				$menuage[l] = File.mtime("#{$basepath}/#{l}/menu.asciidoc")
-				# This is not nice! Invalidate the whole cache
-				$cachedfiles.each { |k, v| v.mtime = Time.at(0) }
-				$menufrags[l] = File.read "#{$cachedir}/#{$latest}/#{l}/menu.html"
-			end
-		}
-	end
-	
 	# Read an existing file from the cache directory or rebuild if necessary
-	# FIXME: Put the menu to its own class
 	def reread
-		rebuild_menu
+		# Block concurrent builds
+		@blocked = true
+		# rebuild_menu
 		@mtime = File.mtime($basepath + @filename)
 		outfile = "#{$cachedir}/#{$latest}/#{@filename}".gsub(/asciidoc$/, "html")
-		lang = @filename[1..2]
-		outdir = "#{$cachedir}/#{$latest}/#{lang}"
+		@lang = @filename[1..2]
+		outdir = "#{$cachedir}/#{$latest}/#{@lang}"
 		cached_mtime = 0
 		cached_exists = false
-		if File.exists?(outfile)
+		if File.exists?(outfile) && @html.nil?
 			cached_mtime = File.mtime(outfile).to_i
-			$stderr.puts "Mtime of cached file: #{cached_mtime}"
-			$stderr.puts "Mtime of asciidoc:    #{@mtime.to_i}"
-			$stderr.puts "Mtime of menu:        #{$menuage[lang].to_i}"
-			cached_exists = true if cached_mtime > @mtime.to_i && cached_mtime > $menuage[lang].to_i
-			$stderr.puts "Using file in cache..." if cached_mtime > @mtime.to_i
+			$stderr.puts "Modification time of file on disk: #{cached_mtime}"
+			$stderr.puts "Modification time of asciidoc:    #{@mtime.to_i}"
+			cached_exists = true if cached_mtime > @mtime.to_i && cached_mtime > $menuage[@lang].to_i
+			$stderr.puts "Using file on disk..." if cached_mtime > @mtime.to_i
 		end
+		cached_exists = false if @filename =~ /menu\.asciidoc$/
 		unless cached_exists
-			lang = @filename[1..2]
-			onthispage = $onthispage[lang]
-			system("asciidoctor -a toc-title=\"#{onthispage}\" -a latest=#{$latest} -a branches=#{$branches} -a branch=#{$latest} -a lang=#{lang} -a jsdir=../../assets/js -a download_link=https://checkmk.com/download -a linkcss=true -a stylesheet=checkmk.css -a stylesdir=../../assets/css -T \"#{$templates}/templates/slim\" -E slim -a toc=right \"#{$basepath}/#{@filename}\" -D \"#{outdir}\"")
-			@html = nil
-		end	
-		if @html.nil?
-			hdoc = Nokogiri::HTML.parse File.new(outfile)
-			$stderr.puts hdoc
-			head  = hdoc.at_css "head"
-			head.add_child "<!-- Hallo Welt -->"
-			mcont = hdoc.css("div[class='main-nav__content']")[0]
-			mcont.inner_html = $menufrags[lang]
-			@html = hdoc.to_s # html(:indent => 4)
+			$stderr.puts "Rebuilding file: " + @filename  
+			onthispage = $onthispage[@lang]
+			if @filename =~ /menu\.asciidoc$/
+				comm = "asciidoctor -T \"#{$templates}/templates/index\" -E slim \"#{$basepath}/#{@lang}/menu.asciidoc\" -D \"#{$cachedir}/#{$latest}/#{@lang}\""
+				$stderr.puts comm
+				system comm
+			else
+				comm = "asciidoctor -a toc-title=\"#{onthispage}\" -a latest=#{$latest} -a branches=#{$branches} -a branch=#{$latest} -a lang=#{@lang} -a jsdir=../../assets/js -a download_link=https://checkmk.com/download -a linkcss=true -a stylesheet=checkmk.css -a stylesdir=../../assets/css -T \"#{$templates}/templates/slim\" -E slim -a toc=right \"#{$basepath}/#{@filename}\" -D \"#{outdir}\""
+				$stderr.puts comm
+				system comm
+			end
 		end
-		
-		# o = Asciidoctor.load_file($basepath + @filename, safe: :unsafe, standalone: true, template_engine: :slim, attributes: $attributes, base_dir: $basepath + "/en", template_dirs: [ $templates + "/templates/slim", $templates + "/templates/index", "/tmp/en"  ] ) # , :docdir => $basepath)
-		
-		# @html = o.convert safe: :server # (:docdir => $basepath)
-		# $stderr.puts "Rebuild cache for #{@filename}"
-		# $stderr.puts o.options
+		@html = File.read(outfile)
+		@blocked = false
 	end
-	
-	
 	
 	# Decide whether to reread or just dump the cached file
 	def to_html
 		$stderr.puts "Checking file: " + $basepath + @filename
-		$stderr.puts "Modification time of asciidoc:    " + File.mtime($basepath + @filename).to_s
-		$stderr.puts "Modification time of cached file: " + @mtime.to_s
+		$stderr.puts "Modification time of asciidoc:             " + File.mtime($basepath + @filename).to_s
+		$stderr.puts "Modification time of file in memory cache: " + @mtime.to_s
 		refresh = false
-		# Check all files for their mtime:
-		[ $basepath + "/de/menu.asciidoc", $basepath + "/en/menu.asciidoc", $basepath + @filename ].each { |f|
-			refresh = true if File.mtime(f) > @mtime
-		}
-		reread if refresh
-		return @html
+		refresh = true if File.mtime($basepath + @filename) > @mtime
+		# Rebuild asciidoc if necessary
+		if refresh == true && @blocked == false
+			reread
+		end
+		# Inject the menu, this will recursively also rebuild if necessary
+		html = @html
+		unless @filename =~ /menu\.asciidoc$/
+			hdoc = Nokogiri::HTML.parse html
+			head  = hdoc.at_css "head"
+			head.add_child "<!-- Hallo Welt -->"
+			mcont = hdoc.css("div[class='main-nav__content']")[0]
+			mcont.inner_html = $cachedfiles["/" + @lang + "/menu.asciidoc"].to_html
+			html = hdoc.to_s # html(:indent => 4)
+		end
+		return html
 	end
 	attr_accessor :mtime
 end
@@ -155,63 +213,49 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
 		response.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, "/latest/en/") if path == "/"
 		response.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, "/latest/en/index.html") if path == "/latest/en/" || path == "/latest/en"
 		response.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, "/latest/de/index.html") if path == "/latest/de/" || path == "/latest/de"
-		# Remove /latest from the path
-		path = path.sub(/^\/latest\//, "/")
-		# Look for a cached file and 
-		if $cachedfiles.has_key? path
-			html = $cachedfiles[path].to_html
-			$stderr.puts "Trying to serve from cache..."
-		elsif path =~ /html$/
-			filename = path.gsub(/html$/, "asciidoc")
-			if File.exists?($basepath + filename)
+		# split the path
+		ptoks = path.strip.split("/")		
+		status = 200
+		ctype = "application/unknown"
+		if $html.include? path.strip
+			if $cachedfiles.has_key? path.strip
+				$stderr.puts "Trying to serve from memory cache..."
+			else
+				filename = "/" + ptoks[-2] + "/" + ptoks[-1].sub(/\.html$/, ".asciidoc")
 				$stderr.puts "Add file to cache #{filename}"
 				s = SingleDocFile.new filename
 				$cachedfiles[path] = s
 			end
-			html = $cachedfiles[path].to_html if $cachedfiles.has_key? path
-		end
-		if html.nil?
-			if path =~ /\/(.*?)\.(css|js|woff|woff2)$/
-				# Search CSS or JS or WOFF in the assets directory
-				content = ""
-				ctype = "text/javascript"
-				# FIXME: pygments can be created in the cache directory...
-				assetfile = $templates + "/" + $1 + "." + $2
-				$stderr.puts "Searching #{assetfile}"
-				if File.exists? assetfile
-					content = File.read assetfile
-				end
-				ctype = "text/css" if path =~ /\.css$/
-				ctype = "font/woff" if path =~ /\.woff$/
-				ctype = "font/woff2" if path =~ /\.woff2$/
-				response.status = 200
-				response.content_type = ctype
-				response.body = content
-			elsif path =~ /images\/(.*)$/
-				image = $1
-				content = ""
-				ctype = "image/png"
-				# We have images both in the assets directory as well as in the docs:
-				[ $basepath + "/images/" + image, $templates + "/assets/images/" + image ].each { |i|
-					if File.exists? i
-						content = File.read i
-					end
-				}
-				ctype = "image/svg+xml" if path =~ /\.svg$/
-				ctype = "image/jpeg" if path =~ /\.jpg$/
-				# FIXME: Serving images this way might only OK for local development
-				response.status = 200
-				response.content_type = ctype
-				response.body = content
-			else
-				response.status = 404
-				response.content_type = "text/plain"
-				response.body = "Ooopsy!"
-			end
-		else
-			response.status = 200
+			html = $cachedfiles[path].to_html
+			response.status = status
 			response.content_type = "text/html"
 			response.body = html
+		end
+		if html.nil? && $allowed.include?(path.strip)
+			if ptoks.include?("assets")
+				# Serve assets directly from the assets directory, first, since assets may contain images	
+				html = File.read $templates + path
+				suffix = ptoks[-1].split(".")[1] 
+				ctype= $mimetypes[suffix] if $mimetypes.has_key? suffix
+			elsif ptoks.include?("images") && ptoks.include?("icons")
+				# Search icons only in the images/icons directory
+				html = File.read $basepath + "/images/icons/" + ptoks[-1]
+				suffix = ptoks[-1].split(".")[1] 
+				ctype= $mimetypes[suffix] if $mimetypes.has_key? suffix
+			elsif ptoks.include?("images")
+				# Search all other images directly in the images directory
+				html = File.read $basepath + "/images/" + ptoks[-1]
+				suffix = ptoks[-1].split(".")[1] 
+				ctype= $mimetypes[suffix] if $mimetypes.has_key? suffix
+			end
+			response.status = status
+			response.content_type = ctype
+			response.body = html
+		end
+		if html.nil?
+			response.status = 404
+			response.content_type = "text/html"
+			response.body = "<html><body>404 File not found!</body></html>"
 		end
 	end
 end
@@ -225,5 +269,6 @@ trap("INT") {
 
 create_config
 prepare_cache
-# rebuild_menu
+prepare_menu
+create_filelist
 server.start
