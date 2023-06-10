f53b9f8e6b7101343051af98ff8f521a6757666f
Mattias Schlenker <ms@mattiasschlenker.de>
2023-06-10 22:22:32 +0200

    fix typo

--- mkp/watterott_co2_ampel/mkp/_agent_based/co2welectronic.py
+++ mkp/watterott_co2_ampel/mkp/_agent_based/co2welectronic.py
@@ -4,7 +4,7 @@
 # © 2022 Mattias Schlenker for tribe29 GmbH
 # © 2023 Mattias Schlenker for Checkmk GmbH
 #
-# Background: The Watterott CO2 Ampel ("CO2 traffic light") is a networkabale
+# Background: The Watterott CO2 Ampel ("CO2 traffic light") is a networkable
 # sensor board, primarily made for monitoring CO2. It is open hardware and uses
 # an open source firmware.
 #
